04dcfd0c9eab18109aa6615ff9d9c114710822eb
usagsing <usagsing@amazon.com>
2024-12-27 20:40:15 +0530

    Added Swagger and comments
    Signed-off-by: usagsing <usagsing@amazon.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
 INSERT INTO employee (emp_id, emp_name, emp_email)
-VALUES ('PS1234', 'Rahul', 'rahul@gmail.com');
+VALUES ('PS1234', 'Rahul Kumar', 'rahul@xyz.com');
+INSERT INTO employee (emp_id, emp_name, emp_email)
+VALUES ('PS0001', 'Nishant Mishra', 'nishant@xyz.com');
